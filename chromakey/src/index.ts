0433de625d42ab6496ea9a976c92b3347627f4c3
PyrozhenkoSerhii <serhii.pyrozhenko@nure.ua>
2022-10-10 18:34:14 +0300

    chromakey update

--- chromakey/src/index.ts
+++ chromakey/src/index.ts
@@ -1,36 +1,48 @@
-require('dear-image.detect-background-color');
-const DearImage = require('dear-image');
-require('canvas');
+import 'dear-image.detect-background-color';
+import 'canvas';
+import { spawn } from 'child_process';
+import DearImage from "dear-image";
 
-const { spawn } = require('child_process');
+const videos = [
+  "https://ssai-s3.nyc3.digitaloceanspaces.com/ads/basketball-2.mp4",
+  "https://ssai-s3.nyc3.digitaloceanspaces.com/ads/basketball-1.webm",
+  "https://ssai-s3.nyc3.digitaloceanspaces.com/ads/3.mp4",
+]
 
-async function main() {
-  const options = [
-    '-i',
-    './assets/3.mp4',
-    '-vframes',
-    '1',
-    '-c:v',
-    'png',
-    '-f',
-    'image2pipe',
-    '-',
-  ];
+const options = (url: string): string[] => [
+  '-i',
+  url,
+  '-vframes',
+  '1',
+  '-c:v',
+  'png',
+  '-f',
+  'image2pipe',
+  '-',
+];
 
-  const buffs: any[] = [];
+ const getMainColor = async (url: string): Promise<string> => {
+  return new Promise((resolve) => {
+    const buffers: any[] = [];
 
-  const p = spawn('ffmpeg', options, { detached: false });
+    const p = spawn('ffmpeg', options(url), { detached: false });
 
-  p.stdout.on('data', (d: any) => {
-    buffs.push(...d);
-  });
+    p.stdout.on('data', (data: any) => {
+      buffers.push(...data);
+    });
 
-  p.stdout.on('end', async (d) => {
-    const bufferImage = Buffer.from(buffs);
+    p.stdout.on('end', async () => {
+      const bufferImage = Buffer.from(buffers);
 
-    const image = await DearImage.loadFrom(bufferImage);
-    const backgroundColor = await DearImage.detectBackgroundColor(image);
-    console.log('backgroundColor', backgroundColor);
-  });
+      const image = await DearImage.loadFrom(bufferImage);
+      const backgroundColor = await DearImage.detectBackgroundColor(image);
+
+      resolve(backgroundColor);
+    });
+  })
 }
-main();
+
+
+videos.forEach(async (v) => {
+  console.log(`URL: ${v} COLOR: `, await getMainColor(v));
+})
